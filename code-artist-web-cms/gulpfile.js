8c008231d66498f8467c3dd19500c945ec78b6f6
AiJiangnan <904629998@qq.com>
2018-01-13 00:32:15 +0800

    添加easyui和gulp

---
+++ code-artist-web-cms/gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp'), //gulp基础库
+    minifycss = require('gulp-minify-css'), //css压缩
+    rename = require('gulp-rename'), //文件重命名
+    uglify = require('gulp-uglify'); //js压缩
+
+gulp.task('default', function() {
+    gulp.start('minifycss', 'minifyjs');
+});
+
+//css压缩
+gulp.task('minifycss', function() {
+    var path = 'css/easyui';
+    return gulp.src(path + '/*.css') //设置css
+        .pipe(rename({ suffix: '.min' })) //修改文件名
+        .pipe(minifycss()) //压缩文件
+        .pipe(gulp.dest(path)); //输出文件目录
+});
+
+//JS压缩
+gulp.task('minifyjs', function() {
+    var path = 'js/easyui';
+    return gulp.src(path + '/*.js') //选择合并的JS
+        .pipe(rename({ suffix: '.min' })) //重命名
+        .pipe(uglify()) //压缩
+        .pipe(gulp.dest(path)); //输出 
+});
