2245815f672b986b14f0b28d4879e95d36027712
AiJiangnan <aijiangnan@xyb.com>
2018-01-15 11:17:51 +0800

    添加log4j和fastJson

--- code-artist-cms/src/main/java/code/artist/cms/core/controller/systam/UserController.java
+++ code-artist-cms/src/main/java/code/artist/cms/core/controller/systam/UserController.java
@@ -4,6 +4,9 @@ import code.artist.common.constants.Constants;
 import code.artist.common.result.RestResponse;
 import code.artist.core.facade.system.IUserService;
 import code.artist.core.model.system.User;
+import com.alibaba.fastjson.JSON;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,14 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("user")
 public class UserController {
+    
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @Autowired
     private IUserService userService;
 
     @RequestMapping(value = "login", method = RequestMethod.POST)
-    public RestResponse login(String username, String password) {
-        System.out.println(username + ":" + password);
-        User loginUser = userService.login(username, password);
+    public RestResponse login(User user) {
+        logger.info("loginInfo: {}", JSON.toJSONString(user));
+        User loginUser = userService.login(user.getUsername(), user.getPassword());
         if (loginUser != null) {
             return new RestResponse<>(Constants.Http.SUCCESS, null, loginUser);
         } else {
