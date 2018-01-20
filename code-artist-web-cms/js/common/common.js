542328cbf541b785e9ff7f637a4eaf8b1f1e6c7c
AiJiangnan <904629998@qq.com>
2018-01-20 12:46:09 +0800

    封装EasyUI

--- code-artist-web-cms/js/common/common.js
+++ code-artist-web-cms/js/common/common.js
@@ -1,15 +1,54 @@
 /**
+ * 常量数据字典
+ */
+var DICT_CONSTANTS = {
+    yesOrNo: ["否", "是"]
+}
+
+/**
  * EasyUI全局配置
  */
 var easyuiConfig = {
-    setPager: function(id) {
+    initDatagrid: function(id) {
         $("#" + id).datagrid({
+            rownumbers: true,
+            pagination: true,
+            singleSelect: true,
             loadMsg: "数据加载中...",
         });
         $("#" + id).datagrid('getPager').pagination({
             beforePageText: '第',
-            afterPageText: '页    共 {pages} 页',
-            displayMsg: '当前显示 {from} - {to} 条记录   共 {total} 条记录'
+            afterPageText: '页 共 {pages} 页',
+            displayMsg: '当前显示 {from} - {to} 条记录 共 {total} 条记录'
         });
+    },
+    setToolbar: function(id, obj) {
+        var toolbarArr = [];
+        if (obj.add) {
+            toolbarArr.push({
+                iconCls: 'icon-add',
+                text: '新建',
+                handler: obj.add
+            });
+        }
+        if (obj.edit) {
+            toolbarArr.push({
+                iconCls: 'icon-edit',
+                text: '编辑',
+                handler: obj.edit
+            });
+        }
+        $("#" + id).datagrid({
+            toolbar: toolbarArr
+        });
+    }
+}
+
+/**
+ * 调用数据字典函数
+ */
+var dict = {
+    yesOrNo: function(value) {
+        return DICT_CONSTANTS.yesOrNo[value];
     }
 }
