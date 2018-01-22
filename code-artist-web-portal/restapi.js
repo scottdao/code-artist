a424e4bef68a9bd3167b12e206fc2840c6b315b0
AiJiangnan <aijiangnan@xyb.com>
2018-01-22 15:30:27 +0800

    删除多余文件

--- code-artist-web-portal/restapi.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var app = express();
-
-app.get('/:id/:name', function (req, res) {
-    res.send(req.params.id + ":" + req.params.name);
-});
-
-var server = app.listen(8081);
