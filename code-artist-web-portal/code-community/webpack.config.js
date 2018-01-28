e6e5c40c2a02a6814debb627a8173e2a2d8e6cbd
scott <249302741@qq.com>
2018-01-28 15:28:39 +0800

    提交前端

--- code-artist-web-portal/code-community/webpack.config.js
+++ code-artist-web-portal/code-community/webpack.config.js
@@ -1,90 +1,126 @@
+//引入路径
 const path = require('path');
-const webpack = require('webpack'); //webpack的安装命令：npm i --save webpack -g
-const openBrowser = require("open-browser-webpack-plugin");
-const htmlWebpackPlugin = require("html-webpack-plugin");
+//引入webpack
+const webpack = require('webpack');
+//引入打开浏览器插件；
+const openBrowser = require('open-browser-webpack-plugin');
+//引入html模板插件；
+const htmlWebpackPlugin = require('html-webpack-plugin');
+//清空文件
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-//配置本地域名；
+//将css文件单独打包到css文件中；
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+//压缩打包；
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
 const webpackServer = {
-    protocol: 'http://',
-    //host:'172.20.0.20',  
-    host: '192.168.0.116',
-    port: '3000'
+	protocol:'http://',
+	host:'172.20.0.31',
+	port:'8080'
 }
-module.exports = {
-    // devtool:"eval-source-map",
-    // 入口文件
-    entry: path.resolve("./src/index.jsx"),
-    output: {
-        path: __dirname + "build",
-        filename: "[name].bundle.js"
-    },
-    devtool: 'inline-source-map',
-    devServer: { //配置Ip服务
-        host: webpackServer.host,
-        inline: true,
-        port: webpackServer.port,
-        contentBase: './build',
-        hot: true
-    },
-    // 自动补全后缀
-    resolve: {
-        extensions: ['.js', ".jsx"],
-        // extensions: ['','.js', ".jsx"]
-    },
-
-    module: {
-        rules: [{
-                test: /\.json$/,
-                use: "json-loader"
-            },
-            {
-                test: /\.(js|jsx)$/,
-                use: "babel-loader"
-            },
-            {
-                test: /\.(css|scss)$/,
-                use: [
-                    "style-loader",
-                    "css-loader",
-                    "sass-loader"
-                ]
-            },
-            {
-                test: /\.(png|jpg|gif|jpeg)$/,
-                use: "file-loader?limit=2048" // 小于2M的图片，进行base64编码
-            },
-            {
-                test: /\.less$/,
-                use: [
-                    "style-loader",
-                    "css-loader",
-                    "less-loader"
-                ]
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: [
-                    'file-loader'
-                ]
-            }
-
-        ]
-    },
-    plugins: [
-        //清除文件
-        new CleanWebpackPlugin('build'),
-        // 文件信息
-        new webpack.BannerPlugin("@Copyright by scott time:2018/01/16"),
-
-        // 自动打开浏览器
-        new openBrowser({
-            url: webpackServer.protocol + webpackServer.host + webpackServer.port
-        }),
-
-        // 配置模板插件
-        new htmlWebpackPlugin({
-            favicon: './favicon.ico',
-            template: __dirname + "/src/index.html"
-        })
-    ]
+module.exports={
+	//入口文件；
+	entry:{
+		vendor: ['react','react-dom','react-router'],
+		app:['./src/index.jsx']
+	},
+	//出口文件；__dirname+'/build/project/'
+	output:{
+		//path:path.join(__dirname,'../','/build/project/'),//配置打包路径
+		path:path.join(__dirname,'/build/project/'),
+		publicPath:'./',//打包后HTML对所有资源链接
+		filename:'js/[name].min.js',
+		chunkFilename:'chuncks/chunkfile.min.js'
+	},
+	resolve:{
+		extensions:['.js','.jsx','json']
+	},
+	module:{
+		rules:[
+			{
+				test:/\.(css|scss)$/,
+//				use:[
+//					"style-loader",
+//					"css-loader",
+//					"sass-loader"
+//				]
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+                    use: [
+                        'css-loader',
+                        'autoprefixer-loader',
+                        'sass-loader'
+                    ]
+				})
+			
+			},
+			{
+				test:/\.(js|jsx)$/,
+				use:"babel-loader"
+			},
+			{
+				test:/\.json$/,
+				use:"json-loader"
+				
+			},
+			{
+				test:/\.less$/,
+//				use:[
+//					"style-loader",
+//					"css-loader",
+//					"less-loader"
+//				]
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+                    use: [
+                        'css-loader',
+                        'autoprefixer-loader',
+                        'less-loader'
+                    ]
+				})	
+			},
+			{
+				test:/\.(png|jpg|gif|jpeg|svg)$/i,
+				use:"file-loader?limit=2048&name=img/[hash:8].[name].[ext]",
+		    }
+		]
+	},
+	devServer: {
+		host: webpackServer.host,
+		port: webpackServer.port,
+		historyApiFallback: true, //不跳转
+	    inline: true,
+	    hot: true,
+	    contentBase:'./build'
+	},
+    devtool: "source-map",
+	plugins:[
+		new CleanWebpackPlugin(['build']//匹配删除的文件
+			,{
+			   root:__dirname,//根目录
+			   verbose:  true,//开启控制台输出信息
+				dry:false//启用删除文件
+			}),
+		//启用UglifyJsPlugin插件进行加速压缩js代码；
+	    new UglifyJsPlugin(),
+    	//生产环境和开发环境对代码压缩的区别；
+	    new webpack.DefinePlugin({
+	            'process.env': {NODE_ENV: '"production"'}
+	        }),
+		new	ExtractTextPlugin({
+		    filename: '[name].min.css',
+		    allChunks: true,
+		}),
+		//html模板插件；
+		new htmlWebpackPlugin({
+			template:__dirname+'/src/index.html',
+			favicon:'./favicon.ico'
+		}),
+		//自动开启浏览器；
+		new openBrowser({
+			url:webpackServer.protocol+webpackServer.host+':'+webpackServer.port
+		}),
+		//文件信息；
+		new webpack.BannerPlugin('@Copyright by scott time:2018')
+	]
 }
