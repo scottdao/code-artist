929524e2199b92bd942c3d1562bcea6e6e2c1164
AiJiangnan <904629998@qq.com>
2018-02-04 15:26:44 +0800

    portal后端框架完成

---
+++ code-artist-service-portal/src/main/java/code/artist/core/service/base/BaseServiceImpl.java
@@ -0,0 +1,39 @@
+package code.artist.core.service.base;
+
+import code.artist.common.base.BaseMapper;
+import code.artist.common.base.BaseModel;
+import code.artist.common.base.IBaseService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Date;
+import java.util.List;
+
+public class BaseServiceImpl<T extends BaseModel> implements IBaseService<T> {
+
+    @Autowired
+    private BaseMapper<T> baseMapper;
+
+    @Override
+    public List<T> selectEntityList() {
+        return baseMapper.selEntityList();
+    }
+
+    @Override
+    public T selectEntityById(Object id) {
+        return baseMapper.selEntityById(id);
+    }
+
+    @Override
+    public int insertEntity(T entity) {
+        Date date = new Date();
+        entity.setCreateTime(date);
+        entity.setUpdateTime(date);
+        return baseMapper.insEntity(entity);
+    }
+
+    @Override
+    public int updateEntityById(T entity) {
+        entity.setUpdateTime(new Date());
+        return baseMapper.updEntityById(entity);
+    }
+}
