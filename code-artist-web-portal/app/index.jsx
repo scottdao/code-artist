1eff7de43714493edac3f2c7ade6bf2d7541153c
scott <249302741@qq.com>
2018-05-29 00:53:22 +0800

    code2

--- code-artist-web-portal/app/index.jsx
+++ code-artist-web-portal/app/index.jsx
@@ -1,20 +1,26 @@
 import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 import {Router,Route,hashHistory} from 'react-router';
-import RouterIndex from './router/router'
-
+import RouterIndex from './pc/router/router'//pc端界面路由
+import MobileRouter from './mobile/router/router'
 //加载antd样式；
-//import 'antd/dist/antd.css'
 
 class Index extends Component{
     constructor(){
         super()
     }
+     componentDidMount(){
+        document.title = '登录';
+
+    }
     render(){
+         let clientFlag = sessionStorage.getItem('clientFlag')
+        //console.log(clientFlag)
+        const clientDom =  clientFlag==1?<MobileRouter />:(clientFlag==2?<RouterIndex />:'客户端出错');
         return(
-             <div className='communityIndex'>
-                <RouterIndex />
-              </div>
+            <React.Fragment>
+                {clientDom}
+            </React.Fragment>
             
         )
     }
---
+++ code-artist-web-portal/app/mobile/router/router.jsx
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+import {Router,Route,IndexRedirect,hashHistory} from 'react-router';
+
+class RouterIndex extends Component{
+    constructor(){
+        super()
+    }
+    render(){
+        return(
+          <div>我是移动端</div>
+        )
+    }
+}
+export default RouterIndex
