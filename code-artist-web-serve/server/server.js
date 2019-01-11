b0980f16473d630bd82d2507881d198c49ad54ca
scottdao <13466484073@139.com>
2019-01-11 18:38:28 +0800

    add

--- code-artist-web-serve/server/server.js
+++ code-artist-web-serve/server/server.js
@@ -1,137 +1,129 @@
-var http = require('http');
-var httpProxy = require('http-proxy');
+var http = require("http");
+var httpProxy = require("http-proxy");
 //console.log(httpProxy);
-var url = require('url');
-var fs = require('fs');
-var querystring = require('querystring');
-var svgCaptcha = require('svg-captcha');
+var url = require("url");
+var fs = require("fs");
+var querystring = require("querystring");
+var svgCaptcha = require("svg-captcha");
 var server = http.createServer();
 
 //console.log(cookieParase);
 var operation = {};
-var dirname  = ''
-operation.fileSys = function(file){
-	dirname = file;
-}
-function post_data(req,res,method){
-	req.on('data',function(chunck){
-						//console.log(chunck.toString());
-						var d = querystring.parse(chunck.toString());
-						console.log(d)
-						var mes = '';
-						switch(method){
-							case 'regist':
-							mes = '注册成功'
-							break;
-							case 'login':
-							mes = '登录成功';
-							break;
-							case 'phoneNumber':
-							mes = '手机号验证成功';
-							break;
-							case 'reVerPswd':
-							mes = '密码设置成功';
-							break;
-							default:
-							break;
-						}
-						
-						domEle(200,res,JSON.stringify({code:200,message:mes,error:null}))
-	})
-}
-function get_data(req,res,urlStr,sdata){
-	
-	domEle(200,res,JSON.stringify({code:200,message:sdata,err:null}))
-	// req.send(sdata);
-}
-server.on('request',function(req,res){
+var dirname = "";
+operation.fileSys = function(file) {
+  dirname = file;
+};
+function post_data(req, res, method) {
+  req.on("data", function(chunck) {
+    //console.log(chunck.toString());
+    var d = querystring.parse(chunck.toString());
+    console.log(d);
+    var mes = "";
+    switch (method) {
+      case "regist":
+        mes = "注册成功";
+        break;
+      case "login":
+        mes = "登录成功";
+        break;
+      case "phoneNumber":
+        mes = "手机号验证成功";
+        break;
+      case "reVerPswd":
+        mes = "密码设置成功";
+        break;
+      default:
+        break;
+    }
 
-	if(req.url !=='/favicon.ico'){
-		
-		var urlStr = url.parse(req.url);
-		//console.log(urlStr.pathname );
-		//console.log(urlStr.pathname==='/user/pswd')
-		//console.log(urlStr);
-		// if(/\/api\/.*$/.test(urlStr.pathname)){
-		// 	  proxy.web(req, res);
-  //       	  return;
-		// }
-		switch (urlStr.pathname) {
-			case '/':
-				var fileH = fs.readFileSync(dirname+'/html/index.html','utf8');
-				domEle(200,res,fileH)
-			break;
-			case '/checkVer/regist'://注册，post请求；
-			//console.log(urlStr.pathname)
-				post_data(req,res,'regist');
-			break;
-			case '/checkVer/login'://登录，post请求；
-				post_data(req,res,'login');
-				// var cookie = req.headers.cookie
-				// console.log(cookie);
-				
-				// req.on('data',function(chunck){
-				// 	var d = querystring.parse(chunck.toString());
-				// 		// console.log(d)
-				// 		res.writeHead(200,{
-				// 			'Content-Type':'text/html;charset=utf-8',
-				// 			'Access-Control-Allow-Origin': 'http://localhost:8080',
-				// 		      "Access-Control-Allow-Credentials":"true",
-				// 		      'Access-Control-Allow-Headers':'X-Requested-With',
-				// 		      "Access-Control-Allow-Methods":"PUT,POST,GET,DELETE,OPTIONS",
-				// 			  'Set-Cookie':'access_token=123489'
-				// 		});
-				// 		res.end();
-				// })
-				//res.write('Set-Cookie:user=daoyun')
-				
-			break;
-			case '/checkVer/phoneNumber'://验证手机号，post请求；
-				post_data(req,res,'phoneNumber');
+    domEle(200, res, JSON.stringify({ code: 200, message: mes, error: null }));
+  });
+}
+function get_data(req, res, urlStr, sdata) {
+  domEle(200, res, JSON.stringify({ code: 200, message: sdata, err: null }));
+  // req.send(sdata);
+}
+server.on("request", function(req, res) {
+  if (req.url !== "/favicon.ico") {
+    var urlStr = url.parse(req.url);
+    //console.log(urlStr.pathname );
+    //console.log(urlStr.pathname==='/user/pswd')
+    //console.log(urlStr);
+    // if(/\/api\/.*$/.test(urlStr.pathname)){
+    // 	  proxy.web(req, res);
+    //       	  return;
+    // }
+    switch (urlStr.pathname) {
+      case "/":
+        var fileH = fs.readFileSync(dirname + "/html/index.html", "utf8");
+        domEle(200, res, fileH);
+        break;
+      case "/checkVer/regist": //注册，post请求；
+        //console.log(urlStr.pathname)
+        post_data(req, res, "regist");
+        break;
+      case "/checkVer/login": //登录，post请求；
+        post_data(req, res, "login");
+        // var cookie = req.headers.cookie
+        // console.log(cookie);
 
-			break;
-			case '/user/pswd'://设置密码，post请求
-			//console.log(1111)
-				post_data(req,res,'reVerPswd');
-			break;
-			case '/graph/verifyCode'://获取图形验证码，get请求；
-				var c = svgCaptcha.create();
-				get_data(req,res,urlStr,{data:c.data});
-			case '/message/verifyCode'://短信验证码，get请求；
-				//console.log(urlStr);
-				var data = querystring.parse(urlStr.query);
-				console.log(data)
-			    get_data(req,res,urlStr,{data:'345678'});
-			default:
-				var fileH = fs.readFileSync(dirname+'/html/404.html','utf8');
-				domEle(404,res,fileH);
-			break;
-		}
-		
-	}
-	
-})
+        // req.on('data',function(chunck){
+        // 	var d = querystring.parse(chunck.toString());
+        // 		// console.log(d)
+        // res.writeHead(200, {
+        //   "Content-Type": "text/html;charset=utf-8",
+        //   "Access-Control-Allow-Origin": "http://localhost:8080",
+        //   "Access-Control-Allow-Credentials": "true",
+        //   "Access-Control-Allow-Headers": "X-Requested-With",
+        //   "Access-Control-Allow-Methods": "PUT,POST,GET,DELETE,OPTIONS",
+        //   "Set-Cookie": "access_token=123489"
+        // });
+        // 		res.end();
+        // })
+        //res.write('Set-Cookie:user=daoyun')
+
+        break;
+      case "/checkVer/phoneNumber": //验证手机号，post请求；
+        post_data(req, res, "phoneNumber");
+
+        break;
+      case "/user/pswd": //设置密码，post请求
+        //console.log(1111)
+        post_data(req, res, "reVerPswd");
+        break;
+      case "/graph/verifyCode": //获取图形验证码，get请求；
+        var c = svgCaptcha.create();
+        get_data(req, res, urlStr, { data: c.data });
+      case "/message/verifyCode": //短信验证码，get请求；
+        //console.log(urlStr);
+        var data = querystring.parse(urlStr.query);
+        console.log(data);
+        get_data(req, res, urlStr, { data: "345678" });
+      default:
+        var fileH = fs.readFileSync(dirname + "/html/404.html", "utf8");
+        domEle(404, res, fileH);
+        break;
+    }
+  }
+});
 var envURLCOR = {
-	dev:"http://localhost:8080",
-	prod:"http://www.baidu.com"
-}
+  dev: "http://localhost:8080",
+  prod: "http://www.baidu.com"
+};
 //console.log(process.env.NODE_ENV)
-function domEle(code,res,content){
-
-
-	res.writeHead(code,{//plain:纯文本；html:解析
-			  'Content-Type':'text/html;charset=utf-8',
-			  'Access-Control-Allow-Origin': envURLCOR.dev,
-		      "Access-Control-Allow-Credentials":"true",
-		      'Access-Control-Allow-Headers':'X-Requested-With',
-		      "Access-Control-Allow-Methods":"PUT,POST,GET,DELETE,OPTIONS",
-			
-		})
-	res.end(content);
-
+function domEle(code, res, content) {
+  res.writeHead(code, {
+    //plain:纯文本；html:解析
+    "Content-Type": "text/html;charset=utf-8",
+    "Access-Control-Allow-Origin": envURLCOR.dev,
+    "Access-Control-Allow-Credentials": "true",
+    "Access-Control-Allow-Headers": "X-Requested-With",
+    "Access-Control-Allow-Methods": "PUT,POST,GET,DELETE,OPTIONS"
+  });
+  res.end(content);
 }
-server.listen(3000,'127.0.0.1',function(){
-	console.log('服务启动...')
+server.listen(3000, "127.0.0.1", function() {
+  console.log("服务启动...");
 });
 
-module.exports=operation
+module.exports = operation;
